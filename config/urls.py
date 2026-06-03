e2e1cd5c74c4db7d33386f1e42069d1ee69e0b0c
Natasha-tech-art <natashabolyn4@gmail.com>
2026-06-03 09:13:23 +0300

    Fix the urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,26 @@
+from django.http import JsonResponse
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+
+def api_root(request):
+    return JsonResponse({
+        'message': 'API is running',
+        'routes': [
+            '/api/register/',
+            '/api/profiles/<id>/',
+            '/api/search/users/',
+            '/api/posts/',
+        ],
+    })
+
 urlpatterns = [
+    path('', api_root),
     path('api/', include('users.urls')),
+    path('api/', include('posts.urls')),
     path('api/login/', TokenObtainPairView.as_view()),
     path('api/refresh/', TokenRefreshView.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,14 +2,12 @@ from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
-class PostListCreateView(
-    generics.ListCreateAPIView
-):
+
+class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    
-    class PostDetailView(
-    generics.RetrieveUpdateDestroyAPIView
-):
-     queryset = Post.objects.all()
+
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
